7172128446cef8083c411425ef41af396f692cd9
Serpopard <ndiramio@gmail.com>
2026-04-22 15:03:24 -0400

    yy

--- server/database.js
+++ server/database.js
@@ -14,9 +14,17 @@ const GameSave = sequelize.define('GameSave', {
   data: { type: DataTypes.TEXT, allowNull: false, defaultValue: '{}' },
 }, { tableName: 'GameSaves', timestamps: true });
 
+const GameScore = sequelize.define('GameScore', {
+  id:       { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  userId:   { type: DataTypes.STRING, allowNull: false },
+  gameSlug: { type: DataTypes.STRING, allowNull: false },
+  score:    { type: DataTypes.INTEGER, allowNull: false },
+  metadata: { type: DataTypes.TEXT, defaultValue: '{}' },
+}, { tableName: 'GameScores', timestamps: true });
+
 async function initDb() {
   await sequelize.authenticate();
   await sequelize.sync({ alter: true });
 }
 
-module.exports = { sequelize, GameSave, initDb };
+module.exports = { sequelize, GameSave, GameScore, initDb };
